cd7328bbf9e81c5e5bbbb26845f122e07d75c6d9
CoderAbhinav <abhinavrb.a02@gmail.com>
2021-10-22 11:32:24 +0530

    added some elements

--- app/src/main/java/com/example/insync/MainActivity.kt
+++ app/src/main/java/com/example/insync/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.insync
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.LoginButton).setOnClickListener{
+            showCredentials();
+        }
+
+
     }
+
+    private fun showCredentials(){
+        val email = findViewById<EditText>(R.id.emailTextField);
+        val psw = findViewById<EditText>(R.id.passwordTextField);
+        val msg = findViewById<TextView>(R.id.helloMessege);
+        msg.text = "Hello, "+ email.text+" ";
+
+    }
+
 }
